839362b4dcf96d83f6c2f3d4377aa0e4a9e267aa
Виктория Давиденко <vikodavido@gmail.com>
2023-02-15 15:21:56 +0200

    homeWork lesson_4

--- patterns/lesson_04/homework_04/appSimpler.js
+++ patterns/lesson_04/homework_04/appSimpler.js
@@ -16,48 +16,52 @@ const input = ["5",
     "5 1 2 4 3"];
 
 const testCases = [];
-let T;
 
-function parseInput() {
-    T = parseInt(input[0]);
+
+function parseInput(input) {
+    let T = parseInt(input[0]);
     let index = 0;
-
+    
+    function preparationTestCase(value) {
+        return value.trim().split(" ").map((a)=>parseInt(a))
+    }
     for (let i = 0; i < T; i++) {
         index++;
-        const [n, q] = input[index]
-            .trim()
-            .split(" ")
-            .map((a) => parseInt(a));
-        index++;
-        const a = input[index]
-            .trim()
-            .split(" ")
-            .map((a) => parseInt(a));
+        const [n, q] = preparationTestCase(input[index])
+        index++; 
+        const a = preparationTestCase(input[index])
+            
         const testCase = {
             n,
             q,
             a,
         };
-
         testCases.push(testCase);
     }
+    return testCases.forEach(solution);
 }
 
-function solution(testCase, index) {
+function solution(testCase) {
     const {n, q, a} = testCase;
+
     let result = "";
     let iq = 0;
-    for (let i = n - 1; i >= 0; i--) {
-        if (a[i] <= iq) result = "1" + result;
-        else if (a[i] > iq && iq < q) {
+
+    function indicator(i) {
+        if (i <= iq) return '1';
+        if (i > iq && iq < q) {
             iq++;
-            result = "1" + result;
-        } else result = "0" + result;
+            return '1'
+        } else return '0'
     }
 
+    for (let i = 0; i < n; i++) {
+        result = indicator(a[i]) + result
+    }
+    
     console.log(result);
+    return result
 }
 
-parseInput();
+parseInput(input);
 
-testCases.forEach(solution);
